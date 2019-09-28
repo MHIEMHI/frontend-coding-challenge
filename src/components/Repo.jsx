008a8b4a91d9ce81457652e8fc89cb2fe748eb06
ehajouji <ehajouji@spacebyte.com>
2019-09-28 15:09:03 +0100

    initial component holder

---
+++ src/components/Repo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Repo extends Component
+{
+	render()
+	{
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
---
+++ src/components/RepoList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class RepoList extends Component
+{
+	render()
+	{
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
